b926b64ffe548c0dccd6d01d878becfd96bc4aa9
Kolja <KKoolljjaa@gmail.com>
2015-12-27 23:43:03 -0600

    added rotation script

---
+++ rotate.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+#
+# rotate_desktop.sh
+#
+# Rotates modern Linux desktop screen and input devices to match. Handy for
+# convertible notebooks. Call this script from panel launchers, keyboard
+# shortcuts, or touch gesture bindings (xSwipe, touchegg, etc.).
+#
+# Using transformation matrix bits taken from:
+#   https://wiki.ubuntu.com/X/InputCoordinateTransformation
+#
+
+# Configure these to match your hardware (names taken from `xinput` output).
+#TOUCHPAD='SynPS/2 Synaptics TouchPad'
+TOUCHSCREEN='12'
+
+rotation=`xrandr -q --verbose|grep eDP1| cut -d ' ' -f6`
+if [ $rotation = "normal" ] ;
+then
+  GO="left"
+else
+  GO="normal"
+fi
+
+
+function do_rotate
+{
+  xrandr --output $1 --rotate $2
+
+  TRANSFORM='Coordinate Transformation Matrix'
+
+  case "$2" in
+    normal)
+      [ ! -z "$TOUCHPAD" ]    && xinput set-prop "$TOUCHPAD"    "$TRANSFORM" 1 0 0 0 1 0 0 0 1
+      [ ! -z "$TOUCHSCREEN" ] && xinput set-prop "$TOUCHSCREEN" "$TRANSFORM" 1 0 0 0 1 0 0 0 1
+      ;;
+    inverted)
+      [ ! -z "$TOUCHPAD" ]    && xinput set-prop "$TOUCHPAD"    "$TRANSFORM" -1 0 1 0 -1 1 0 0 1
+      [ ! -z "$TOUCHSCREEN" ] && xinput set-prop "$TOUCHSCREEN" "$TRANSFORM" -1 0 1 0 -1 1 0 0 1
+      ;;
+    left)
+      [ ! -z "$TOUCHPAD" ]    && xinput set-prop "$TOUCHPAD"    "$TRANSFORM" 0 -1 1 1 0 0 0 0 1
+      [ ! -z "$TOUCHSCREEN" ] && xinput set-prop "$TOUCHSCREEN" "$TRANSFORM" 0 -1 1 1 0 0 0 0 1
+      ;;
+    right)
+      [ ! -z "$TOUCHPAD" ]    && xinput set-prop "$TOUCHPAD"    "$TRANSFORM" 0 1 0 -1 0 1 0 0 1
+      [ ! -z "$TOUCHSCREEN" ] && xinput set-prop "$TOUCHSCREEN" "$TRANSFORM" 0 1 0 -1 0 1 0 0 1
+      ;;
+  esac
+}
+
+XDISPLAY=`xrandr --current | grep primary | sed -e 's/ .*//g'`
+XROT=`xrandr --current --verbose | grep primary | egrep -o ' (normal|left|inverted|right) '`
+
+do_rotate $XDISPLAY $GO
+
+if [ ! -z "$2" ]; then
+  sleep $2
+  do_rotate $XDISPLAY $XROT
+  exit 0
+fi
+
